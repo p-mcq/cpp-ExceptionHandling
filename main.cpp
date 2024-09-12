a184abfe7e29bd69af82e6ba52945e9580801a61
Paul McQuaid <104009739+p-mcq@users.noreply.github.com>
2024-09-12 14:19:24 +0200

    Practice at handling multiple possible errors from a function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int miles{0};
+int gallons{0};
+int milesPerGallon{0};
+
+double CalculateMilesPerGallon(int _miles, int _gallons)
+{
+    if (_gallons == 0 || _miles == 0)
+    {
+        throw invalid_argument("Divide by zero error");
+    }
+
+    if (_gallons < 0 || _miles < 0)
+    {
+        throw invalid_argument("Negative value error");
+    }
+
+    return static_cast<double>(_miles) / _gallons;
+}
+
 int main()
 {
+    cout << "Enter the miles: ";
+    cin >> miles;
+    cout << "\nEnter the gallons: ";
+    cin >> gallons;
+    try
+    {
+        milesPerGallon = CalculateMilesPerGallon(miles, gallons);
+        cout << "\nResult: " << milesPerGallon << endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    cout << "Bye" << endl;
     return 0;
 }
