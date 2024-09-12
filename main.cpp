7f94dc79bd9c745eaa6072bfc263c6da69cf193d
Paul McQuaid <104009739+p-mcq@users.noreply.github.com>
2024-09-12 15:06:25 +0200

    Practice creating user defined exception classes and throwing errors on class constructors

--- main.cpp
+++ main.cpp
@@ -1,4 +1,31 @@
+#include <iostream>
+#include <memory>
+#include "Account.h"
+#include "Checking_Account.h"
+#include "Savings_Account.h"
+#include "Trust_Account.h"
+#include "IllegalBalanceException.H"
+
 int main()
 {
+    try
+    {
+        std::unique_ptr<Account> a1 = std::make_unique<Checking_Account>("Moe", -10);
+        std::cout << *a1 << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch (const IllegalBalanceException &e)
+    {
+        std::cerr << "Error Creating Account - Negative Balance" << '\n';
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown Error" << '\n';
+    }
+
+    std::cout << "Program Terminated" << std::endl;
     return 0;
 }
